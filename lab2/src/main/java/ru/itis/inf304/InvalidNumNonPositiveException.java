17c773903e454d07f234c3bda783f83b86c30595
makarjaan <154356563+makarjaan@users.noreply.github.com>
2024-02-24 01:37:58 +0300

    Delete lab2 directory

--- lab2/src/main/java/ru/itis/inf304/InvalidNumNonPositiveException.java
+++
@@ -1,7 +0,0 @@
-package ru.itis.inf304;
-
-public class InvalidNumNonPositiveException extends Exception{
-    public InvalidNumNonPositiveException() {
-        super("Число не должно быть меньше нуля");
-    }
-}
--- lab2/src/main/java/ru/itis/inf304/NumberLessMinException.java
+++
@@ -1,7 +0,0 @@
-package ru.itis.inf304;
-
-public class NumberLessMinException extends Exception{
-    public NumberLessMinException(){
-        super("Вы ввели число меньше минимального в массиве");
-    }
-}
